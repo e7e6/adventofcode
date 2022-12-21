46176af82438ddb45f6d9dadfc54c9cbb74d28ea
Anthony Nowocien <e7e6@users.noreply.github.com>
2022-12-21 17:06:31 +0100

    First part... still has to be corrected

--- 2022/day15.sql
+++ 2022/day15.sql
@@ -1,47 +1,40 @@
--- WORK IN PROGRESS
+drop table if exists input;
+create table input(id integer generated always as identity, line text);
 
--- test data
-\copy input (line) from 'day15_test.input'
+--\copy input (line) from 'day15_test.input'
+\copy input (line) from 'day15.input'
 
--- Sensor at x=2, y=18: closest beacon is at x=-2, y=15   
--- Sensor at x=9, y=16: closest beacon is at x=10, y=16   
--- Sensor at x=13, y=2: closest beacon is at x=15, y=3    
--- Sensor at x=12, y=14: closest beacon is at x=10, y=16  
--- Sensor at x=10, y=20: closest beacon is at x=10, y=16  
--- Sensor at x=14, y=17: closest beacon is at x=10, y=16  
--- Sensor at x=8, y=7: closest beacon is at x=2, y=10
--- Sensor at x=2, y=0: closest beacon is at x=2, y=10
--- Sensor at x=0, y=11: closest beacon is at x=2, y=10    
--- Sensor at x=20, y=14: closest beacon is at x=25, y=17  
--- Sensor at x=17, y=20: closest beacon is at x=21, y=22  
--- Sensor at x=16, y=7: closest beacon is at x=15, y=3    
--- Sensor at x=14, y=3: closest beacon is at x=15, y=3    
--- Sensor at x=20, y=1: closest beacon is at x=15, y=3    
+-------------------------------------------------------
+-- FIRST PART... 
+-- Incorrect, but I so far fail to understand why
+-------------------------------------------------------
+--4408541 to low
 
--------------------------------------------------------   
--- FIRST PART
--------------------------------------------------------   
-
-
-with dataprep as (
-select id,
-       (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\1'))::int as x_sensor,
-       (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\2'))::int as y_sensor,
-       (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\3'))::int as x_beacon,
-       (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\4'))::int as y_beacon
-from input) ,
-geo as (
-select (x_sensor || ',' || y_sensor)::point as sensor,
-       (x_beacon || ',' || y_beacon)::point as beacon,
-       circle((x_sensor || ',' || y_sensor)::point, abs(x_beacon - x_sensor) + abs(y_beacon - y_sensor)) as circle
-from dataprep),
-range as (
-select least(MIN(x_sensor), MIN(x_beacon)) as minx,
-       greatest(MAX(x_sensor), MAX(x_beacon)) as maxx
-from dataprep)
-select i, 
-       bool_or(circle @> point(i,20)) 
-from generate_series((select minx from range),(select maxx from range)) i 
-cross join geo 
-where point(i,20) not in (select beacon from geo) -- not the proper operator
-group by i order by i;
+with dataprep as
+(
+        select id,
+               (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\1'))::int as x_sensor,
+               (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\2'))::int as y_sensor,
+               (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\3'))::int as x_beacon,
+               (regexp_replace(line, 'Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)', '\4'))::int as y_beacon
+        from input
+) ,
+range as
+(
+        select least(MIN(x_sensor), MIN(x_beacon)) as minx,
+               greatest(MAX(x_sensor), MAX(x_beacon)) as maxx
+        from dataprep
+)
+,
+check_exists as
+(
+        select i,
+               bool_or(abs(i - x_sensor) + abs(2000000 - y_sensor) <= abs(x_beacon - x_sensor) + abs(y_beacon - y_sensor)
+                       and not exists (select 1 from dataprep where x_beacon=i and y_beacon=2000000)) as presence
+        from generate_series((select minx from range),(select maxx from range)) i
+        cross join dataprep
+        group by i
+)
+select count(*)
+from check_exists
+where presence;
